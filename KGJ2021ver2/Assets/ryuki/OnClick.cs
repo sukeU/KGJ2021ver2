fd9cc326784ab6c6f72d9b99411ac75cdbb3db46
DESKTOP-O08MK87ryuki <ryuuki.nomura@icloud.com>
2021-09-08 22:55:41 +0900

    タイトル終了ボタン

---
+++ KGJ2021ver2/Assets/ryuki/OnClick.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnClick: MonoBehaviour
+{
+    // Start is called before the first frame update
+    //void Start()
+    //{
+        public void EscOnClick()
+        {
+            Debug.Log("ÉQÅ[ÉÄÇèIóπÇµÇ‹Ç∑");
+            Application.Quit();
+        }
+    //}
+
+    // Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+}

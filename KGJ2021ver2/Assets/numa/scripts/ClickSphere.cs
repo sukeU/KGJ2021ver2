8b23caa892ac92f1c9dbcef024a3a6e8541db1b4
DESKTOP-0MOG2PB\kenke <lovecraftkeke@icloud.com>
2021-09-09 13:49:41 +0900

    キャラクター設置

--- KGJ2021ver2/Assets/numa/scripts/ClickSphere.cs
+++ KGJ2021ver2/Assets/numa/scripts/ClickSphere.cs
@@ -2,23 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ClickSphere : MonoBehaviour
+public class ClikMouseButton : MonoBehaviour
 {
-    public GameObject prefab;
+    public GameObject kangaroo;
     private Vector3 mousePosition;
-    GetClickedGameObject GetClickedGameObject;
+    private GameObject clickedGameObject;
+    private Vector3 mousePos;
 
     private void Start()
     {
-        GetClickedGameObject = GetComponent<GetClickedGameObject>();
     }
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)&& GetClickedGameObject.clickedGameObject.CompareTag("miti"))
+        if (Input.GetMouseButtonDown(0))
         {
-            mousePosition = Input.mousePosition;
-            mousePosition.z = 10.0f;
-            Instantiate(prefab, Camera.main.ScreenToWorldPoint(mousePosition), Quaternion.identity);
+            clickedGameObject = null;
+
+            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+
+            //ヒットした2Dオブジェクトを入れておく
+            Collider2D click_miti = Physics2D.OverlapPoint(mousePos, LayerMask.GetMask("miti"));
+            Collider2D click_wall = Physics2D.OverlapPoint(mousePos, LayerMask.GetMask("wall"));
+            if (click_miti)
+            {
+                clickedGameObject = click_miti.transform.gameObject;
+                mousePosition = Input.mousePosition;
+                mousePosition.z = 10.0f;
+                Instantiate(kangaroo, Camera.main.ScreenToWorldPoint(mousePosition), Quaternion.identity);
+            }
+            if (click_wall)
+            {
+                clickedGameObject = click_wall.transform.gameObject;
+            }
+
+            Debug.Log("tag" + clickedGameObject.layer);
+            
         }
     }
 }

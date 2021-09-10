c31299dbc7ae2054c63ae3f591f6ce5d59380136
2023150-PC\2023150 <daigakukait2023@gmail.com>
2021-09-10 14:49:41 +0900

    EnemySystemにゴールオブジェクトを含ませた

--- KGJ2021ver2/Assets/sukeU/Script/GoalS.cs
+++ KGJ2021ver2/Assets/sukeU/Script/GoalS.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class GoalS : MonoBehaviour
 {
+    public static bool GameOver = false;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Enemy enemy = collision.GetComponent<Enemy>();
         if (enemy != null)
         {
+            GameOver = true;
             SceneManagerS SceneManager = GameObject.Find("SceneManager").GetComponent<SceneManagerS>();
             SceneManager.LoadResult();
         }
